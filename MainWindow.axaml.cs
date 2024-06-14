739c64b040a6d27240bdacf064752b4cfd795ddf
ZvarykinM <zvarykinmk@oiate.ru>
2024-06-14 16:36:02 +0300

    Вроде работает, но не совсем верно

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,16 +1,21 @@
 using Avalonia.Controls;
 using ScottPlot.Avalonia;
 using Avalonia.Interactivity;
+using System.Collections.Generic;
+using KVP = System.Collections.Generic.KeyValuePair<string, ScottPlot.Color>;
+using ScottPlot;
+using Avalonia;
 namespace PipeHandler2;
 
 public partial class MainWindow : Window
 {
-    // private AvaPlot gridPlot;
-
-    //private Robot PipeRobot;
-
-    private double FingerRad; 
-
+    private Dictionary<string, Color> HoleStatesDescription = new(new List<KVP>([new KVP("Done", Colors.DarkGreen),
+                                                                                 new KVP("Finger", Colors.Brown),
+                                                                                 new KVP("Planned", Colors.Yellow),
+                                                                                 new KVP("Goal", Colors.Orange),
+                                                                                 new KVP("Inaccessible", Colors.Red),
+                                                                                 new KVP("Accessible", Colors.Blue),
+                                                                                 new KVP("Neutral", Colors.LightBlue)]));
     private TextBlock NameOfPipeDeskLabel;
 
     private CheckBox AutoRegimeFlagCheckBox;
@@ -20,24 +25,133 @@ public partial class MainWindow : Window
     private TextBlock StateInfoLabel;
 
     private AvaPlot gridPlot;
+
+    private const string ConstPathToFile ="КуАЭС";
+
+    private string[] States = ["Prev", "Curr", "Future"];
+
+    private Robot PipeRobot;
+
+    private void DrawHoles()
+    {
+        if(PipeRobot is not null)
+        {
+            foreach(var pair in PipeRobot.GetGridContext.CoordsInfo)
+            {
+                var C = gridPlot.Plot.Add.Circle(xCenter: pair.Key[0], yCenter: pair.Key[1], radius: PipeRobot.Radius);
+                C.FillColor = HoleStatesDescription[pair.Value];
+                C.LineColor = Colors.Black;
+            }
+            gridPlot.Plot.Axes.SetLimitsX(PipeRobot.GetGridContext.GetXBoards[0], PipeRobot.GetGridContext.GetXBoards[1]);
+            gridPlot.Plot.Axes.SetLimitsY(PipeRobot.GetGridContext.GetYBoards[0], PipeRobot.GetGridContext.GetYBoards[1]);
+            gridPlot.Refresh();
+        }
+    }
+    
+    private void Initialization(int[] F0, int[] F1, int[] F2, string Path = ConstPathToFile)
+    {
+        PipeRobot = new(F0, F1, F2, Path);
+        DrawHoles();
+        foreach(var state in States)
+            DrawState(state);
+        gridPlot.Refresh();
+    }
+
+    private int[] F0 = [6, 14];
+    private int[] F1 = [6, 2];
+    private int[] F2 = [7, 9];
+
+    private List<ScottPlot.Plottables.Ellipse[]> StateFingersSchemas;
+
+    private List<ScottPlot.Plottables.LinePlot[]> StateHandsSchemas;
+    
+    private void DrawFingers(Robot.RobotState SomeRobotState)
+    {
+        var FingerCircleSchemas = new ScottPlot.Plottables.Ellipse[3];
+        FingerCircleSchemas[0] = gridPlot.Plot.Add.Circle(xCenter: SomeRobotState.MoveableFingers[0].HandPos.X, yCenter: SomeRobotState.MoveableFingers[0].HandPos.Y, radius: PipeRobot.Radius / 1.5);
+        FingerCircleSchemas[1] = gridPlot.Plot.Add.Circle(xCenter: SomeRobotState.MoveableFingers[1].HandPos.X, yCenter: SomeRobotState.MoveableFingers[1].HandPos.Y, radius: PipeRobot.Radius / 1.5);
+        FingerCircleSchemas[2] = gridPlot.Plot.Add.Circle(xCenter: SomeRobotState.F2.X, yCenter: SomeRobotState.F2.Y, radius: PipeRobot.Radius / 1.5);
+        FingerCircleSchemas[0].FillColor = Colors.Red;
+        FingerCircleSchemas[1].FillColor = Colors.Blue;
+        FingerCircleSchemas[2].FillColor = Colors.Green;
+    }
+
+    private void DrawState(string State)
+    {
+        Robot.RobotState RS;
+        ScottPlot.Plottables.LinePlot[] Hands;
+        Color HandsCol;
+        LinePattern HandsPattern;
+        int LineWidth;
+        switch(State)
+        {
+            case "Prev":
+                RS = PipeRobot.GetRobotStates[0];
+                Hands = StateHandsSchemas[0];
+                HandsCol = Colors.Black;
+                HandsPattern = LinePattern.Dashed;
+                LineWidth = 3;
+                break;
+            case "Curr":
+                RS = PipeRobot.GetRobotStates[1];
+                Hands = StateHandsSchemas[1];
+                HandsCol = Colors.Black;
+                HandsPattern = LinePattern.Solid;
+                LineWidth = 3;
+                break;
+            case "Future":
+                RS = PipeRobot.GetRobotStates[2];
+                Hands = StateHandsSchemas[1];
+                HandsCol = Colors.DeepPink;
+                HandsPattern = LinePattern.Solid;
+                LineWidth = 2;
+                break;
+            default: throw new System.Exception("НЕИЗВЕСТНОЕ СОСТОЯНИЕ");
+        }
+        if(RS is not null)
+        {
+            DrawFingers(RS);
+            Hands[0] = gridPlot.Plot.Add.Line(RS.MoveableFingers[0].HandPos.X, RS.MoveableFingers[0].HandPos.Y, RS.F2.X, RS.F2.Y);
+            Hands[1] = gridPlot.Plot.Add.Line(RS.MoveableFingers[1].HandPos.X, RS.MoveableFingers[1].HandPos.Y, RS.F2.X, RS.F2.Y);
+            foreach(var h in Hands)
+            {
+                h.LineColor = HandsCol;
+                h.LinePattern = HandsPattern;
+                h.LineWidth = LineWidth;
+            }
+        }
+    }
+
     public MainWindow()
     {
         InitializeComponent();
-        var gridPlot = this.Find<AvaPlot>("GridPlot");
+        StateFingersSchemas = new();
+        for(var i = 0; i < 3; i++)
+            StateFingersSchemas.Add(new ScottPlot.Plottables.Ellipse[3]);
+        StateHandsSchemas = new();
+        for(var i = 0; i < 3; i++)
+            StateHandsSchemas.Add(new ScottPlot.Plottables.LinePlot[2]);
+        gridPlot = this.Find<AvaPlot>("GridPlot");
         InputJsonPath = this.Find<TextBox>("InputPathToJson");
         NameOfPipeDeskLabel = this.Find<TextBlock>("PipeDeskName");
         StateInfoLabel = this.Find<TextBlock>("StateInfo");
         AutoRegimeFlagCheckBox = this.Find<CheckBox>("CheckForAuto");
+        gridPlot.Plot.Axes.SquareUnits();
+        Initialization(F0, F1, F2);
     }
 
     public void ClickRenew(object sender, RoutedEventArgs args)
     {
-        
+        PipeRobot.NewState([2, 0], [2, 0], [2, 0]);
+        foreach(var state in States)
+            DrawState(state);
+        gridPlot.Refresh();
     }
 
     public void EnterPathToFile(object sender, RoutedEventArgs args)
     {
-        
+        gridPlot.Plot.Clear();
+        Initialization(F0, F1, F2, InputJsonPath.Text);
     }
 
     public void ShowFinalState(object sender, RoutedEventArgs args)
--- Robot.cs
+++ Robot.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 namespace PipeHandler2;
 
 class Robot
@@ -20,7 +21,12 @@ class Robot
     private const double robL0Min = 0.0623;
     private const double robL0Max = 0.2696;
 
-    public double StepX, StepY, Radius;    
+    public double StepX, StepY, Radius;
+
+    private Grid GridContext;
+
+    public Grid GetGridContext{get => GridContext; private set{}}
+
     public class Coordinate
     {
         public double X, Y;
@@ -43,14 +49,14 @@ class Robot
     {
         public Coordinate[] handLine{get; set;}
         public Coordinate HandPos{get; set;}
-        public int[] IntHandPos{get; set;}
         public double handLen{get; set;}
     }
 
     public class RobotState: ICloneable
     {
         public Coordinate F2{get; set;}
-        public Hand[] MoveableFingers{get; set;} //0 - Fing0, 1 - Fing1, 2 - FingH
+        private Hand[] SetMoveableFingers = new Hand[2];
+        public Hand[] MoveableFingers{get => SetMoveableFingers; set => SetMoveableFingers = value;} //0 - Fing0, 1 - Fing1, 2 - FingH
         public Coordinate Center;
         public object Clone() => new RobotState(){F2 = F2, MoveableFingers = MoveableFingers, Center = Center};
         public RobotState Copy
@@ -60,8 +66,25 @@ class Robot
         }
     }
 
+    private RobotState[] SetOfRobotStates = new RobotState[3];
+    public RobotState[] GetRobotStates{get => SetOfRobotStates;}
     private int Iterator = 0;
     public int CurrIterationIndex{get => Iterator; private set{}}
+
+    public Robot(int[] F0, int[] F1, int[] F2, string PathToFile)
+    {
+        GridContext = new(PathToFile);
+        StepX = GridContext.XStep;
+        StepY = GridContext.YStep;
+        Radius = GridContext.Radius;
+        SetOfRobotStates = new RobotState[3];
+        var f0 = new double[2]{F0[0] * StepX, F0[1] * StepY};
+        var f1 = new double[2]{F1[0] * StepX, F1[1] * StepY};
+        var f2 = new double[2]{F2[0] * StepX, F2[1] * StepY};
+        var StartState02 = Inversion02(null, f2, f0);
+        var StartState = Inversion1(StartState02, f1);
+        SetOfRobotStates[1] = StartState;
+    }
 
     public static Coordinate Ein(double alpha) => new(){X = Math.Cos(alpha), Y = Math.Sin(alpha)};
     public static Coordinate EinNeg(double alpha) => -1 * Ein(alpha);
@@ -70,13 +93,15 @@ class Robot
     public static double AngNorm(double alpha) => alpha >= 0 ? alpha : 2 * Math.PI + alpha;
     public Coordinate IntCoordToDouble(int[] IndCoord) => new Coordinate(){X = IndCoord[0] * StepX, Y = IndCoord[1] * StepY};
 
-    public RobotState Inversion02(RobotState? SomeRobotState, int[] ij2, int[] ij0)
+    public RobotState? Inversion02(RobotState? SomeRobotState, double[] ij2, double[] ij0)
     {
-        var XY2 = new Coordinate(){X = StepX * ij2[0], Y = StepY * ij2[1]};
-        var XY0 = new Coordinate(){X = StepX * ij0[0], Y = StepY * ij0[1]};
+        var XY2 = new Coordinate(ij2);
+        var XY0 = new Coordinate(ij0);
+        Coordinate Dxy, c;
+        Hand h0;
         if(ij2[0] <= ij0[0])
         {
-            var Dxy = XY0 - XY2;
+            Dxy = XY0 - XY2;
             var theta = Math.Atan2(Dxy.Y, Dxy.X);
             var alpha = Math.Asin((robF0x - robF2x) / Dxy.Norm);
             if(theta is not double.NaN)
@@ -85,9 +110,9 @@ class Robot
                 var l0 = e0 * Dxy - robF2y;
                 var e1 = Compl(e0);
                 var q2 = Aff(e1, robF2x, XY2);
-                var c = Aff(e1, robC2x, Aff(e0, -robC2y, q2));
+                c = Aff(e1, robC2x, Aff(e0, -robC2y, q2));
                 var q0 = Aff(e0, robF2y, q2);
-                var h0 = new Hand(){handLine = [e0, q0], HandPos = XY0, IntHandPos = ij2, handLen = l0};
+                h0 = new Hand(){handLine = [e0, q0], HandPos = XY0, handLen = l0};
                 var NewRobotState = SomeRobotState is not null ? SomeRobotState.Copy : new RobotState();
                 NewRobotState.F2 = XY2;
                 NewRobotState.MoveableFingers[0] = h0;
@@ -98,7 +123,7 @@ class Robot
         }
         else
         {
-            var Dxy = XY2 - XY0;
+            Dxy = XY2 - XY0;
             var theta = Math.Atan2(Dxy.Y, Dxy.X);
             var alpha = Math.Sin((robF0x - robF2x) / Dxy.Norm);
             if(theta is not double.NaN)
@@ -107,9 +132,9 @@ class Robot
                 var l0 = e0 * Dxy - robF2y;
                 var e1 = Compl(e0);
                 var q2 = Aff(e1, -robF2x, XY2);
-                var c = Aff(e1, -robC2x, Aff(e0, robC2y, q2));
+                c = Aff(e1, -robC2x, Aff(e0, robC2y, q2));
                 var q0 = Aff(e0, -robF2y, q2);
-                var h0 = new Hand(){handLine = [e0, q0], HandPos = XY0, IntHandPos = ij0, handLen = -l0};
+                h0 = new Hand(){handLine = [e0, q0], HandPos = XY0, handLen = -l0};
                 var NewRobotState = SomeRobotState is not null ? SomeRobotState.Copy : new RobotState();
                 NewRobotState.F2 = XY2;
                 NewRobotState.MoveableFingers[0] = h0;
@@ -120,9 +145,9 @@ class Robot
         }
     }
 
-    public RobotState Inversion1(RobotState SomeRobotState, int[] ij1)
+    public RobotState? Inversion1(RobotState SomeRobotState, double[] ij1)
     {
-        var XY1 = new Coordinate(){X = StepX * ij1[0], Y = StepY * ij1[1]};
+        var XY1 = new Coordinate(ij1);
         var c_xy = SomeRobotState.Center;
         var z = robF1x + robC1x;
         if(XY1.Y < c_xy.Y)
@@ -138,7 +163,7 @@ class Robot
                 var e1 = Compl(e0);
                 var q0 = Aff(e0, -robC1y, Aff(e1, -robC1x, c_xy));
                 var l1 = D_xy * e0 - robC1y;
-                var h = new Hand(){handLine = [e0, q0], HandPos = XY1, IntHandPos = ij1, handLen = -l1};
+                var h = new Hand(){handLine = [e0, q0], HandPos = XY1, handLen = -l1};
                 var NewRobotState = SomeRobotState.Copy;
                 NewRobotState.MoveableFingers[1] = h;
                 return NewRobotState;
@@ -158,7 +183,7 @@ class Robot
                 var e1 = Compl(e0);
                 var q0 = Aff(e0, robC1y, Aff(e1, robC1x, c_xy));
                 var l1 = D_xy * e0 - robC1y;
-                var h = new Hand(){handLine = [e0, q0], HandPos = XY1, IntHandPos = ij1, handLen = l1};
+                var h = new Hand(){handLine = [e0, q0], HandPos = XY1, handLen = l1};
                 var NewRobotState = SomeRobotState.Copy;
                 NewRobotState.MoveableFingers[1] = h;
                 return NewRobotState;
@@ -167,14 +192,14 @@ class Robot
         }
     }
 
-    public RobotState InversionH(RobotState SomeRobotState, int[] ij1)
+    public RobotState? InversionH(RobotState SomeRobotState, double[] ij1)
     {
-        var XY1 = new Coordinate(){X = StepX * ij1[0], Y = StepY * ij1[1]};
+        var XY1 = new Coordinate(ij1);
         var c_xy = SomeRobotState.Center;
         var z = robC1x + robHx;
         if(XY1.Y < c_xy.Y)
         {
-            var D_xy = c_xy - XY1; //new Coord(){X = c_xy.X - XY1.X, Y = c_xy.Y - XY1.Y};
+            var D_xy = c_xy - XY1;
             var d = D_xy.Norm;
             var beta = Math.Acos(D_xy.X / d);
             var alpha = Math.Asin(z / d);
@@ -185,9 +210,9 @@ class Robot
                 var e1 = Compl(e0);
                 var q0 = Aff(e0, -robC1y, Aff(e1, -robC1x, c_xy));
                 var l1 = D_xy * e0 - robC1y;
-                var hh = new Hand(){handLine = [e0, q0], HandPos = XY1, IntHandPos = [Convert.ToInt32(XY1.X / StepX), Convert.ToInt32(XY1.Y / StepY)], handLen = -l1};
+                var hh = new Hand(){handLine = [e0, q0], HandPos = XY1, handLen = -l1};
                 var q1 = Aff(e1, -robF1x, Aff(e0, - l1 + robHy, q0));
-                var h1 = new Hand(){handLine = [e0, q0], HandPos = q1, IntHandPos = [Convert.ToInt32(q1.X / StepX), Convert.ToInt32(q1.Y / StepY)], handLen = -l1 + robHy};
+                var h1 = new Hand(){handLine = [e0, q0], HandPos = q1, handLen = -l1 + robHy};
                 var NewRobotState = SomeRobotState.Copy;
                 NewRobotState.MoveableFingers[2] = hh;
                 NewRobotState.MoveableFingers[1] = h1;
@@ -209,9 +234,9 @@ class Robot
                 var e1 = Compl(e0);
                 var q0 = Aff(e0, robC1y, Aff(e1, robC1x, c_xy));
                 var l1 = D_xy * e0 - robC1y;
-                var hh = new Hand(){handLine = [e0, q0], HandPos = XY1, IntHandPos = [Convert.ToInt32(XY1.X / StepX), Convert.ToInt32(XY1.Y / StepY)], handLen = l1};
+                var hh = new Hand(){handLine = [e0, q0], HandPos = XY1, handLen = l1};
                 var q1 = Aff(e1, robF1x, Aff(e0, l1 - robHy, q0));
-                var h1 = new Hand(){handLine = [e0, q0], HandPos = q1, IntHandPos = [Convert.ToInt32(q1.X / StepX), Convert.ToInt32(q1.Y / StepY)], handLen = l1 - robHy};
+                var h1 = new Hand(){handLine = [e0, q0], HandPos = q1, handLen = l1 - robHy};
                 var NewRobotState = SomeRobotState.Copy;
                 NewRobotState.MoveableFingers[3] = hh;
                 NewRobotState.MoveableFingers[1] = h1;
@@ -221,4 +246,98 @@ class Robot
         }
     }
 
+    public bool CheckHand(Hand H, double lmin, double lmax)
+    {
+        var l = Math.Abs(H.handLen) / 1900; //(hand_flag == false ? 1000 : 1); //??? ОТКУДА ПАРАМЕТРЫ НА L_MIN, L_MAX ???
+        return l > lmin && l < lmax;
+    }
+
+    public bool CheckF0(RobotState SomeRobotState) => CheckHand(SomeRobotState.MoveableFingers[0], robL0Min, robL0Max);
+
+    public bool CheckF1(RobotState SomeRobotState) => CheckHand(SomeRobotState.MoveableFingers[1], robL1Min, robL1Max);
+
+    // public bool CheckF0(RobotState SomeRobotState) => CheckHand("F0", SomeRobotState);
+
+    // public bool CheckF1(RobotState SomeRobotState) => CheckHand("F1", SomeRobotState);
+
+    // public bool CheckHand(string FingIndex, RobotState SomeRobotState)
+    // {
+    //     double lmin = 0.0, lmax = 0.0, len = 0.0;
+    //     if(FingIndex == "FH")
+    //     {
+    //         (lmin, lmax) = (robHy + robL1Min, robHy + robL1Max);
+    //         len = SomeRobotState.MoveableFingers[2].handLen / 1900;
+    //     }
+    //     else if(FingIndex == "F0")
+    //     {
+    //         (lmin, lmax) = (robL0Min, robL0Max);
+    //         len = SomeRobotState.MoveableFingers[0].handLen / 1900;
+    //     }
+    //     else if(FingIndex == "F1")
+    //     {
+    //         (lmin, lmax) = (robL1Min, robL1Max);
+    //         len = SomeRobotState.MoveableFingers[1].handLen / 1900;
+    //     }
+    //     return len > lmin && len < lmax;
+    // }
+
+    public void IntermidiateInversion(double[] f2, double[] f0, double[] f1)
+    {
+        var NewRobotState02 = Inversion02(SetOfRobotStates[2], f2, f0);
+        if(NewRobotState02 is not null)
+        {
+            SetOfRobotStates[0] = SetOfRobotStates[1];
+            SetOfRobotStates[1] = SetOfRobotStates[2];
+            var flagFing0 = CheckF0(NewRobotState02);
+            if(flagFing0)
+            {
+                var NewRobotState1 = Inversion1(NewRobotState02, f1);
+                var flagFing1 = CheckF1(NewRobotState1);
+                if(flagFing1)
+                    SetOfRobotStates[2] = NewRobotState1;
+                else SetOfRobotStates[2] = null;
+            }
+        }
+    }
+
+    public void StartInversion(double[] f2, double[] f0, double[] f1)
+    {
+        var NewRobotState02 = Inversion02(SetOfRobotStates[1], f2, f0);
+        if(NewRobotState02 is not null)
+        {
+            SetOfRobotStates[0] = SetOfRobotStates[1];
+            SetOfRobotStates[1] = NewRobotState02;
+            var flagFing0 = CheckF0(NewRobotState02);
+            if(flagFing0)
+            {
+                var NewRobotState1 = Inversion1(NewRobotState02, f1);
+                var flagFing1 = CheckF1(NewRobotState1);
+                if(flagFing1)
+                    SetOfRobotStates[2] = NewRobotState1;
+                else SetOfRobotStates[2] = null;
+            }
+        }
+    }
+
+    public void Inversion(double[] f2, double[] f0, double[] f1)
+    {
+        if(Iterator == 0)
+            StartInversion(f2, f0, f1);
+        else IntermidiateInversion(f2, f0, f1);
+        Iterator++;
+    }
+
+    public (double[], double[], double[]) NewPosForState(int[] f0_aff, int[] f1_aff, int[] f2_aff)
+    {
+        double[] Pos0 = [SetOfRobotStates[1].MoveableFingers[0].HandPos.X + f0_aff[0] * StepX, SetOfRobotStates[1].MoveableFingers[0].HandPos.Y + f0_aff[1] * StepY];
+        double[] Pos1 = [SetOfRobotStates[1].MoveableFingers[1].HandPos.X + f1_aff[0] * StepX, SetOfRobotStates[1].MoveableFingers[1].HandPos.Y + f1_aff[1] * StepY];
+        double[] Pos2 = [SetOfRobotStates[1].F2.X + f2_aff[0] * StepX, SetOfRobotStates[1].F2.Y + f2_aff[1] * StepY];
+        return (Pos0, Pos1, Pos2);
+    }
+
+    public void NewState(int[] f0_aff, int[] f1_aff, int[] f2_aff)
+    {
+        var NewPos = NewPosForState(f0_aff, f1_aff, f2_aff);
+        Inversion(NewPos.Item3, NewPos.Item1, NewPos.Item2);
+    }
 }
